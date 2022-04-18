cb2759f6d7119a8c7365ee0a2d167979dda49f37
Jeff Larson <jeff_larson@byu.edu>
2022-04-18 12:12:15 -0600

    Redo autograder for 20 points

--- Lesson_2_Assignment_Tests.R
+++ Lesson_2_Assignment_Tests.R
@@ -5,9 +5,14 @@ library(testthat)
 # you can have multiple tests for each question
 
 
-test_that("Q5 (visible)", {
+test_that("Q5 Factored (visible)", {
   
   expect_true(is.factor(Majors$Major))
+
+})
+
+test_that("Q5 Ordered (visible)", {
+  
   expect_true(is.ordered(Majors$Major))
   
 })
@@ -21,11 +26,16 @@ test_that("Q8 (visible)", {
   
 })
 
-test_that("Q9 (visible)", {
+test_that("Q9 Subsetted (visible)", {
   
   expect_equal(as.numeric(unname(marketing[1,1])), 20.7, tolerance = 1e-2) 
   expect_equal(as.numeric(unname(marketing[3,1])), 22.4, tolerance = 1e-2) 
   expect_equal(as.numeric(unname(marketing[4,1])), 22.8, tolerance = 1e-2) 
+
+})
+
+test_that("Q9 Major Format (visible)", {
+  
   expect_true(is.factor(marketing$Major))
   expect_true(is.ordered(marketing$Major))
   
@@ -47,7 +57,17 @@ test_that("Q11 (visible)", {
   expect_true(length(stratYN) == 19)
 })
 
-test_that("Q12 (visible)", {
+test_that("Q12 Subsetted (visible)", {
+  
+  expect_equal(as.numeric(unname(strategy[1,1])), 19.3, tolerance = 1e-2) 
+  expect_equal(as.numeric(unname(strategy[3,1])), 23.1, tolerance = 1e-2) 
+  expect_equal(as.numeric(unname(strategy[4,1])), 23.3, tolerance = 1e-2) 
+  expect_true(is.factor(strategy$Major))
+  expect_true(is.ordered(strategy$Major))
+  
+})
+
+test_that("Q12 Major Format (visible)", {
   
   expect_equal(as.numeric(unname(strategy[1,1])), 19.3, tolerance = 1e-2) 
   expect_equal(as.numeric(unname(strategy[3,1])), 23.1, tolerance = 1e-2) 
@@ -63,11 +83,16 @@ test_that("Q13 (visible)", {
   
 })
 
-test_that("Q14 (visible)", {
+test_that("Q14 Subsetted (visible)", {
   
   expect_equal(as.numeric(unname(entrepreneurship[1,1])), 21.0, tolerance = 1e-2) 
   expect_equal(as.numeric(unname(entrepreneurship[2,1])), 21.4, tolerance = 1e-2) 
   expect_equal(as.numeric(unname(entrepreneurship[3,1])), 22.2, tolerance = 1e-2) 
+
+})
+
+test_that("Q14 Major Format (visible)", {
+  
   expect_true(is.factor(entrepreneurship$Major))
   expect_true(is.ordered(entrepreneurship$Major))
   
@@ -91,19 +116,35 @@ test_that("Q17 (visible)", {
   
 })
 
-test_that("Q18 (visible)", {
+test_that("Q18 List1 (visible)", {
   
   expect_equal(MajorList[[1]][1, 1], 20.71259, tolerance = 1e-3) 
   expect_equal(MajorList[[1]][2, 1], 21.80737, tolerance = 1e-3) 
   expect_equal(MajorList[[1]][5, 1], 23.01437, tolerance = 1e-3) 
+
+})
+
+test_that("Q18 List2 (visible)", {
+  
   expect_equal(MajorList[[2]][2, 1], 22.34075, tolerance = 1e-3) 
+
+})
+
+test_that("Q18 List3 (visible)", {
+  
   expect_equal(MajorList[[3]][1, 1], 21.00289, tolerance = 1e-3) 
   expect_equal(MajorList[[3]][3, 1], 22.22459, tolerance = 1e-3) 
   
 })
 
-test_that("Q19 (visible)", {
+test_that("Q19 Age (visible)", {
   
   expect_equal(as.numeric(unname(agegenmark3[1])), 22.43433, tolerance = 1e-3) 
-  expect_true(is.factor(unname(agegenmark3$Gender)))
+
 })
+test_that("Q19 Gender (visible)", {
+  
+  expect_true(is.factor(unname(agegenmark3$Gender)))
+  
+})
+
